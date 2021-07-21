f5c6dbedf6acb5e1660da184e764b1336392bdfd
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-07-21 23:29:03 +0530

    building neural network

--- training.py
+++ training.py
@@ -31,3 +31,26 @@ for intent in intents['intents']:
 words = [lematizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
+classes = sorted(set(classes))
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(words,open('classes.pkl','wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lematizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag,output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:,0])
+train_y = list(training[:,1])
